903cf7f8c6508527a28c008d94d3e10544d08987
vlelyavin <vlelyavin@gmail.com>
2023-03-09 22:42:23 +0200

    config modified

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/luna_edge_test_task",
-  assetPrefix: `/${"luna_edge_test_task"}/`,
   reactStrictMode: true,
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,6 @@
 import { useState } from "react";
 import { SearchBar } from "../components/SearchBar";
 import { MovieList } from "../components/MovieList";
-import { useDispatch, useSelector } from "react-redux";
-import { SelectedPageInterface, StateInterface } from "../typescript/interfaces";
-import ReactPaginate from "react-paginate";
-import { setError, setLoadingStatus, setMovies } from "../actions/actions";
-import { Loader } from "../components/Loader";
 import { Pagination } from "../components/Pagination/Pagination";
 
 export default function Home() {
